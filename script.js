397a31afccac9133947b71958362228e43a08c18
MuaazAttari <160422454+MuaazAttari@users.noreply.github.com>
2024-09-25 21:32:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let number = +prompt('Enter number to Generate Multiplication table:');
+
+if (number > 0) {
+
+for (let i = 0; i < 11; i++) {
+    console.log(i*number);
+}
+}
+else{
+    for (let i = 0; i < 11; i++) {
+        console.log(i*3);
+    }   
+}
